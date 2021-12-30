b8d5ff3226bf1e3609203fb5229c7fe3a152f1c6
JT <jtfernandes.it@gmail.com>
2021-12-30 15:06:11 -0300

    bug fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO"
+    return "Laboratório DevOps - FIAP"
 
 if __name__ == '__main__':
     app.run()
