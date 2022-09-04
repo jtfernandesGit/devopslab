3cb77f35ed87be7da0c1f8697926bdfe3c45d768
jtfernandes <jtfernandes.it@gmail.com>
2022-09-04 15:44:28 -0300

    update pipeline

--- test.py
+++ test.py
@@ -17,4 +17,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):                                                                                                                                                                             
         # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório DevOps - MBA FIAP V3")
+        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório DevOps - MBA FIAP V4")
